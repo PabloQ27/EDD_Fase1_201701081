22d3797dc963fe1401f5f04685baf36f65b39955
PabloQ27 <3001519500101@ingenieria.usac.edu.gt>
2021-12-13 02:02:19 -0600

    avl

---
+++ js/calendario.js
@@ -0,0 +1,36 @@
+class Calendario{
+    constructor(hora, dia, mes, evento){
+        this.hora = hora
+        this.dia = dia
+        this.mes = mes 
+        this.evento = evento
+    }
+}
+
+class Nodo1{ //para cabeceras
+    constructor(obj){
+        this.obj = obj
+        this.sig = null
+        this. = null
+    }
+}
+class Lista1{
+    constructor(){
+        this.cabeza = null
+    }
+}
+
+class Nodo2{//Nodos internos
+    constructor(obj){
+        this.obj = obj
+        this.ant = null
+        this.sig = null
+    }
+}
+
+class Lista2{
+    constructor(){
+        this.cabeza = null
+    }
+}
+
--- js/empleado.js
+++ js/empleado.js
@@ -16,32 +16,109 @@ class AVL{
         let nuevo = new Nodo(obj)
         if (this.raiz == null){
             this.raiz = nuevo
+            console.log("alguito",this.raiz.obj.id)
         }else{
-            this.insertar_nodo(this.raiz, nuevo)
+            this.insertar_nodo(nuevo, this.raiz)
 
         }
     }
 
-    insertar_nodo(raiz_actual, nuevo){
+    insertar_nodo(nuevo, raiz_actual){
         if(raiz_actual != null){ // entra si hay nodo  y esta lleno
-            if (nuevo.obj < raiz_actual.obj){
-                raiz_actual.izq = this.insertar_nodo(raiz_actual.izq, nuevo) 
+            if (nuevo.obj.id < raiz_actual.obj.id){
+                raiz_actual.izq = this.insertar_nodo(nuevo, raiz_actual.izq) 
                 
-            }else if (nuevo.obj > raiz_actual.obj){
-                raiz_actual.der = this.insertar_nodo(raiz_actual.der, nuevo)
+                if((this.altura(raiz_actual.izq)-this.altura(raiz_actual.der)) == 2){
+                    
+                    if(nuevo.obj.id < raiz_actual.izq.obj.id){
+                        raiz_actual = this.srl(raiz_actual)
+                        
+                    }else{
+                        raiz_actual = this.drl(raiz_actual)
+                    }
+                }
+            }else if (nuevo.obj.id > raiz_actual.obj.id){
+                raiz_actual.der = this.insertar_nodo(nuevo, raiz_actual.der)
+                if ((this.altura(raiz_actual.der)-this.altura(raiz_actual.izq)) == 2){
+                    if (nuevo.obj.id > raiz_actual.der.obj.id){
+                        raiz_actual = this.srr(raiz_actual)
+                        
+                    }else{
+                        raiz_actual = this.drr(raiz_actual)
+                    }
+                }
             }else{
                 console.log("No se permiten valores repetidos")
                 
-            }
+            }     
             return raiz_actual
         }else{
             raiz_actual = nuevo
+            var r = this.altura(raiz_actual.der)
+            var l = this.altura(raiz_actual.izq)
+            var m = this.max(r,l)
+            raiz_actual.altura = m + 1
             return raiz_actual
         }
 
     }
 
+    altura(raiz_actual){
+        if(raiz_actual == null){
+            return -1
+        }else{
+            return raiz_actual.altura
+        }
+    }
+
+    max(val1,val2){
+        if (val1 > val2){
+            return val1
+        }else{
+            return val2
+        }
+    }
+
+    srl(t1){
+        var t2
+        t2 = t1.izq
+        t1.izq = t2.der
+        t2.der = t1
+        t1.altura = max(this.altura(t1.izq),this.altura(t1.der)) + 1
+        t2.altura = max(this.altura(t2.izq), t1.altura) + 1
+        return t2
+    }
+
+    drl(raiz_actual){
+        raiz_actual.izq = this.srr(raiz_actual.izq)
+        return this.srl(raiz_actual)
+    }
+
+    srr(t1){
+        var t2
+        t2 = t1.der
+        t1.der = t2.izq
+        t2.izq = t1
+        t1.altura = this.max(this.altura(t1.izq), this.altura(t1.der)) + 1
+        t2.altura = this.max(this.altura(t2.der), t1.altura) + 1
+        return t2
+    }
+
+    drr(raiz_actual){
+        raiz_actual.der = this.srl(raiz_actual.der)
+        return this.srr(raiz_actual)
+    }
+
+    preorder(temp) {
+            if (temp != null) {
+                console.log(temp.obj.id)
+                this.preorder(temp.izq)
+                this.preorder(temp.der)
+            }
+        }
 }
+
+
 
 class Empleado{
     constructor(id, nombre, edad, correo, password, listaClientes){
@@ -56,13 +133,25 @@ class Empleado{
     
 }
 
-let empleado = new Empleado(1,"dsd","dsd","dsd","dsd","dsd")
-let empleado = new Empleado(1,"dsd","dsd","dsd","dsd","dsd")
-let empleado = new Empleado(1,"dsd","dsd","dsd","dsd","dsd")
-let empleado = new Empleado(1,"dsd","dsd","dsd","dsd","dsd")
-let empleado = new Empleado(1,"dsd","dsd","dsd","dsd","dsd")
+let empleado1 = new Empleado(5,"dsd","dsd","dsd","dsd","dsd")
+let empleado2 = new Empleado(10,"dsd","dsd","dsd","dsd","dsd")
+let empleado3 = new Empleado(20,"dsd","dsd","dsd","dsd","dsd")
+let empleado4 = new Empleado(25,"dsd","dsd","dsd","dsd","dsd")
+let empleado5 = new Empleado(30,"dsd","dsd","dsd","dsd","dsd")
+let empleado6 = new Empleado(35,"dsd","dsd","dsd","dsd","dsd")
+let empleado7 = new Empleado(40,"dsd","dsd","dsd","dsd","dsd")
 let avl = new AVL()
-avl.insertar(empleado)
+avl.insertar(empleado1)
+avl.insertar(empleado2)
+avl.insertar(empleado3)
+avl.insertar(empleado4)
+avl.insertar(empleado5)
+avl.insertar(empleado6)
+avl.insertar(empleado7)
+avl.preorder(avl.raiz)
+
+
+
 /* function ver(){
     console.log("abeer")
     let empleado1 = new Empleado(23,'roberto',44, 'correossss','passsss','liss')
